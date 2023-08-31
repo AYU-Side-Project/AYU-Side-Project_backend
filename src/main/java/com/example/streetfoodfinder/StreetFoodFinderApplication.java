b01093cc3dedfee1c1dd26a9241f36817aa3a56b
esunsu <esunsu@naver.com>
2023-08-31 16:09:05 +0900

    init

---
+++ src/main/java/com/example/streetfoodfinder/StreetFoodFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.streetfoodfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetFoodFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreetFoodFinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/streetfoodfinder/StreetFoodFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.streetfoodfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreetFoodFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
