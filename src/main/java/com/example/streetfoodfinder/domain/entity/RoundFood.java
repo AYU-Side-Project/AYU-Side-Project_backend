7bc101b31f6c9f1829edb4dbdff2afc1beb2433e
MinDi <123048828+alswl0912@users.noreply.github.com>
2024-01-30 01:27:04 +0900

    Update RoundFood.java
    @DaTa , @getter , @Setter , @NoArgsConstructor , @AllArgsConstructor 등 어노테이션 추가 및 코드 간결화

--- src/main/java/com/example/streetfoodfinder/domain/entity/RoundFood.java
+++ src/main/java/com/example/streetfoodfinder/domain/entity/RoundFood.java
@@ -5,38 +5,26 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
+@Getter
+@Setter
 public class RoundFood {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String imageUrl;
-    private String nextRoundInfo; //다음 라운드 정보 저장 필드
+    private String nextRoundInfo; // 다음 라운드 정보 저장 필드
 
-    public RoundFood() { //JPA 엔티티 기본 생성자
+    public RoundFood() { // JPA 엔티티 기본 생성자
     }
 
-    public RoundFood(String imageUrl, String nextRoundInfo) { //이미지 URL과 다음 라운드 정보를 받아 객체를 초기화
+    public RoundFood(String imageUrl, String nextRoundInfo) { // 이미지 URL과 다음 라운드 정보를 받아 객체를 초기화
         this.imageUrl = imageUrl;
         this.nextRoundInfo = nextRoundInfo;
     }
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getImageUrl() {
-        return imageUrl;
-    }
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-    public String getNextRoundInfo() {
-        return nextRoundInfo;
-    }
-    public void setNextRoundInfo(String nextRoundInfo) {
-        this.nextRoundInfo = nextRoundInfo;
-    }
+
 }
