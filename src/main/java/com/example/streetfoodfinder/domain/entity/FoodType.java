7f5801da6b66cdeb618a80798ba3922d960233b0
MinDi <123048828+alswl0912@users.noreply.github.com>
2024-01-30 01:25:18 +0900

    Update FoodType.java
    @DaTa , @getter , @Setter , @NoArgsConstructor , @AllArgsConstructor 등 어노테이션 추가 및 코드 간결화

--- src/main/java/com/example/streetfoodfinder/domain/entity/FoodType.java
+++ src/main/java/com/example/streetfoodfinder/domain/entity/FoodType.java
@@ -4,37 +4,27 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FoodType {
-    private static final String S3BUCKETURL = "https://sft-worldcup.s3.ap-northeast-2.amazonaws.com/food/"; //S3 버킷 URL 상수 선언
+    private static final String S3BUCKETURL = "https://sft-worldcup.s3.ap-northeast-2.amazonaws.com/food/"; // S3 버킷 URL 상수 선언
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String imageUrl; //음식 이미지 URL
-
-    public FoodType() { //JPA 엔티티 기본 생성자
-
-    }
+    private String imageUrl; // 음식 이미지 URL
 
     public FoodType(String imageName) {
-        this.imageUrl = imageName; //이미지 이름 받아 음식 객체 생성
+        this.imageUrl = imageName; // 이미지 이름 받아 음식 객체 생성
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-
-    public String getFoodImageUrl(String foodName) {     // 음식 이미지 URL 출력
+    public String getFoodImageUrl(String foodName) { // 음식 이미지 URL 출력
         return S3BUCKETURL + foodName + ".jpg";
     }
 }
