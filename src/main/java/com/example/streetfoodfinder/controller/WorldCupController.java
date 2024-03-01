90dfcce8a031dc2b17636b27ab563e6cd16e724d
MinDi <123048828+alswl0912@users.noreply.github.com>
2024-03-02 05:44:30 +0900

    Update WorldCupController.java
    다음 라운드 진행하는 엔드포인트 삭제

--- src/main/java/com/example/streetfoodfinder/controller/WorldCupController.java
+++ src/main/java/com/example/streetfoodfinder/controller/WorldCupController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 public class WorldCupController {
 
     private final WorldCupService worldCupService;
+    private FoodType[] randomFoods;
 
     public WorldCupController(WorldCupService worldCupService) {
         this.worldCupService = worldCupService;
@@ -19,19 +20,7 @@ public class WorldCupController {
 
     @GetMapping("/randomfood")
     public ResponseEntity<FoodType[]> getRandomFood() {
-        FoodType[] randomFoods = worldCupService.getRandomImages();
+        randomFoods = worldCupService.getRandomImages();
         return new ResponseEntity<>(randomFoods, HttpStatus.OK);
-    }
-
-    @GetMapping("/selectedfood/{index}") // 선택한 음식 로직 호출
-    public ResponseEntity<String> selectFood(@PathVariable int index) {
-        worldCupService.processSelectedFood(index);
-        return new ResponseEntity<>("Food selected successfully", HttpStatus.OK);
-    }
-
-    @GetMapping("/nextround") // 다음 라운드로 진행하는 엔드포인트
-    public ResponseEntity<FoodType[]> getNextRoundFood() {
-        FoodType[] nextRoundFoods = worldCupService.getNextRoundImages();
-        return new ResponseEntity<>(nextRoundFoods, HttpStatus.OK);
     }
 }
