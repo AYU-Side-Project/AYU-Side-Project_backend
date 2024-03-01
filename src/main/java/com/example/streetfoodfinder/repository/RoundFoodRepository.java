c722551ecec9c2a64d5144cbe66b52477e34b7e8
MinDi <123048828+alswl0912@users.noreply.github.com>
2024-03-02 05:49:17 +0900

    Delete src/main/java/com/example/streetfoodfinder/repository/RoundFoodRepository.java

--- src/main/java/com/example/streetfoodfinder/repository/RoundFoodRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.streetfoodfinder.repository;
-
-import com.example.streetfoodfinder.domain.entity.RoundFood;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoundFoodRepository extends JpaRepository<RoundFood, Long> {
-}
