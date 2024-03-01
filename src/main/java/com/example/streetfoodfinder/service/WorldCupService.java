53edb08f57d83d5e973f5f780d8ad6994238fb39
MinDi <123048828+alswl0912@users.noreply.github.com>
2024-03-02 05:50:02 +0900

    Update WorldCupService.java

--- src/main/java/com/example/streetfoodfinder/service/WorldCupService.java
+++ src/main/java/com/example/streetfoodfinder/service/WorldCupService.java
@@ -1,17 +1,12 @@
 package com.example.streetfoodfinder.service;
 
 import com.example.streetfoodfinder.domain.entity.FoodType;
-import com.example.streetfoodfinder.domain.entity.RoundFood;
-import com.example.streetfoodfinder.repository.RoundFoodRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Random;
 
 @Service
-@Transactional
 public class WorldCupService {
 
     @Value("${s3.bucket.url}")
@@ -28,16 +23,23 @@ public class WorldCupService {
             "samgyeopsal.jpg",
             "susi.jpg",
             "toppokki.jpg",
-            "udon.jpg"
+            "udon.jpg",
+            "jjimdark.jpg",
+            "naengmyeon.jpg",
+            "gejang.jpg",
+            "kimchibokkeumbab.jpg",
+            "darkgalbi.jpg"
     };
 
-    private final RoundFoodRepository roundFoodRepository;
+    public FoodType[] getRandomImages() {
+        int[] randomIndices = getRandomIndices();
+        FoodType food1 = new FoodType(s3BucketUrl + imageUrls[randomIndices[0]]);
+        FoodType food2 = new FoodType(s3BucketUrl + imageUrls[randomIndices[1]]);
 
-    public WorldCupService(RoundFoodRepository roundFoodRepository) {
-        this.roundFoodRepository = roundFoodRepository;
+        return new FoodType[]{food1, food2};
     }
 
-    public FoodType[] getRandomImages() {
+    private int[] getRandomIndices() {
         Random random = new Random();
         int index1 = random.nextInt(imageUrls.length);
         int index2;
@@ -46,55 +48,7 @@ public class WorldCupService {
             index2 = random.nextInt(imageUrls.length);
         } while (index1 == index2);
 
-        if (imageUrls.length > index1 && imageUrls.length > index2) {
-            FoodType food1 = new FoodType(s3BucketUrl + imageUrls[index1]);
-            FoodType food2 = new FoodType(s3BucketUrl + imageUrls[index2]);
-
-            return new FoodType[]{food1, food2};
-        } else {
-            return new FoodType[0];
-        }
+        return new int[]{index1, index2};
     }
 
-    public void processSelectedFood(int selectedIndex) {
-        FoodType[] randomImages = getRandomImages();
-
-        if (selectedIndex >= 0 && selectedIndex < 2) {
-            FoodType selectedFood = (selectedIndex == 0) ? randomImages[0] : randomImages[1];
-
-            if (selectedFood != null) {
-                RoundFood roundFood = new RoundFood();
-                roundFood.setImageUrl(selectedFood.getImageUrl());
-                roundFoodRepository.save(roundFood);
-            } else {
-                throw new RuntimeException("Selected food is null");
-            }
-        } else {
-            throw new IllegalArgumentException("Invalid index: " + selectedIndex);
-        }
-    }
-
-    public FoodType[] getNextRoundImages() {
-        List<RoundFood> winners = roundFoodRepository.findAll();
-
-        if (winners.size() >= 2) {
-            RoundFood winner1 = winners.get(0);
-            RoundFood winner2 = winners.get(1);
-
-            FoodType nextRoundFood1 = new FoodType(winner1.getImageUrl());
-            FoodType nextRoundFood2 = new FoodType(winner2.getImageUrl());
-
-            RoundFood nextRoundResult1 = new RoundFood(nextRoundFood1.getImageUrl(), "다음 라운드 정보 1");
-            RoundFood nextRoundResult2 = new RoundFood(nextRoundFood2.getImageUrl(), "다음 라운드 정보 2");
-
-            roundFoodRepository.save(nextRoundResult1);
-            roundFoodRepository.save(nextRoundResult2);
-
-            roundFoodRepository.deleteAll(winners);
-
-            return new FoodType[]{nextRoundFood1, nextRoundFood2};
-        }
-
-        return null;
-    }
 }
